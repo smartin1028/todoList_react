be29abb1694b78e0b6c19463369203446176a388
daewon <leedw3232@gmail.com>
2022-05-17 21:15:40 +0900

    hook useCallBack use

--- src/component/Todos.tsx
+++ src/component/Todos.tsx
@@ -2,14 +2,14 @@ import TodoHeader from "./TodoHeader";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 import TodoFooter from "./TodoFooter";
-import {useRef, useState} from "react";
+import {useCallback, useRef, useState} from "react";
 import {Todo} from "../App";
 
 const Todos = () => {
         const nextId = useRef(1);
         const [todos, setTodos] = useState<Todo[]>([]);
 
-        const handleInsert = (text: string) => {
+        const handleInsert = useCallback((text: string) => {
             const todo = {
                 id: nextId.current,
                 text,
@@ -20,29 +20,31 @@ const Todos = () => {
 
             // 성능 향상을 위해서 컴포넌트의 상태 변경을 함수형 업데이트 방식으로 바꾼다?
             // 함수형 업데이트
-            setTodos((todos) => todos.concat(todo) );
+            setTodos((todos) => todos.concat(todo));
             // setTodos((todos) => {return todos.concat(todo)});
             nextId.current += 1;
-        }
+        },[]);
+
+
 
         // const handleRemove = (id:number)
+        const aaa = useCallback(() => {
 
-        const handleRemove = (id: number) => {
-            function showLog() {
-                console.log("====================1======================")
-                console.log(todos.filter((todo) => todo.id !== id));
-                console.log("====================2======================")
-                console.log(todos.filter((todo) => todo.id === id));
-                console.log("====================3======================")
-            }
+        },[]);
 
-            showLog();
+        const handleRemove = useCallback((id: number) => {
+
+            console.log("====================1======================")
+            console.log(todos.filter((todo) => todo.id !== id));
+            console.log("====================2======================")
+            console.log(todos.filter((todo) => todo.id === id));
+            console.log("====================3======================")
 
             // setTodos(todos.filter((todo) => todo.id !== id))
-
             setTodos((todos) => todos.filter((todo) => todo.id !== id))
-        };
-        const handleToggle = (id: number) => {
+        },[]);
+
+        const handleToggle = useCallback((id: number) => {
             // setTodos(
             //     todos.map((todo) =>
             //             todo.id === id ? {...todo, done: !todo.done} : todo
@@ -58,20 +60,17 @@ const Todos = () => {
                 todos.map((todo) => todo.id === id ? {...todo, done: !todo.done} : todo)
             );
 
-            function checkFunction() {
-                let test
-                todos.map((todo) => todo.id === id ? (test = {...todo, done: !todo.done}) : todo)
-                console.log(test)
-            }
+            // data check 함수 결과 확인용
+            let test
+            todos.map((todo) => todo.id === id ? (test = {...todo, done: !todo.done}) : todo)
+            console.log(test)
 
-               // data check 함수 결과 확인용
-//             checkFunction();
 
-        }
+        },[]);
 
-        const handleClearAll = () => {
-            setTodos(()=>[]);
-        }
+        const handleClearAll = useCallback(() => {
+            setTodos(() => []);
+        },[]);
 
         return (
             <div>
